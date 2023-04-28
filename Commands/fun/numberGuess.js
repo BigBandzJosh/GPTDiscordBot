63bc518950d7e2a06a8fb5326cc337bf61bf76c1
Josh <113129413+BigBandzJosh@users.noreply.github.com>
2023-04-28 14:25:31 -0300

    NumberGuesser doesn't want to take my number and tell me if it's right or not, just pops out guess a number between 1 and 10

--- Commands/fun/numberGuess.js
+++ Commands/fun/numberGuess.js
@@ -6,29 +6,41 @@ module.exports = {
         .setName('numberguess')
         .setDescription('Replies with a number guessing game!'),
     async execute(interaction) {
-     // Generates a random number between 1 and 10
-const randomNumber = 1;
+     const randomNumber = Math.floor(Math.random() * 10) + 1;
 
-// Starts a loop with 3 attempts
-let attempts = 3;
-// While there are attempts left
-while (attempts > 0) {
-    // Asks the user for a number
-    const userNumber = await interaction.reply('Guess a number between 1 and 10');
-    //track specific user attempts and decrease attempts each time
+     let attempts = 3;
 
-    //if user is correct, break out of loop
-    if (userNumber === randomNumber) {
-        await interaction.editReply('You guessed the number!' + randomNumber);
-        break;
-    }
-    // If the user didn't guess the number
-    else {
-        await interaction.editReply(`Wrong number! You have ${attempts} attempts left` + randomNumber);
-        attempts--;
+     while (attempts > 0) {
+            const filter = (m) => {
+                if (m.author.id !== interaction.user.id) return false;
+                const int = parseInt(m.content);
+                return !isNaN(int) && int >= 1 && int <= 10;
+            };
+            const message = await interaction.reply("Guess a number between 1 and 10");
+            
+            const guess = await interaction.options.getString({
+                filter,
+                max: 1,
+                time: 10000,
+                errors: ['time']
+
+            });
+            if (guess.size === 0)
+                 await interaction.editReply("You didn't reply in time!");
+                break;
+            }
+            const userChoice = parseInt(guess.first().content);
+            if (userChoice === randomNumber) {
+                await interaction.editReply("You got it right!");
+            } else {
+                await interaction.editReply(`You got it wrong! The number was ${randomNumber}`);
+                attempts--;
+                if (attempts > 0) {
+                    await interaction.editReply(`You have ${attempts} attempts left!`);
+                } else {
+                    await interaction.editReply("You have no attempts left!");
+                }
+            }
+        }
     }
 
-    }
-    },
-};
-    
