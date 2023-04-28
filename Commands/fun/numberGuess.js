6e75430499b7be2ed60453066ff59eed19eac394
Josh <113129413+BigBandzJosh@users.noreply.github.com>
2023-04-28 14:50:41 -0300

    number guesser still broken

--- Commands/fun/numberGuess.js
+++ Commands/fun/numberGuess.js
@@ -4,43 +4,29 @@ const {SlashCommandBuilder} = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('numberguess')
-        .setDescription('Replies with a number guessing game!'),
-    async execute(interaction) {
-     const randomNumber = Math.floor(Math.random() * 10) + 1;
+        .setDescription('Replies with a number guessing game!')
+        .addStringOption(option =>
+            option.setName('number')
+                .setDescription('The number to guess')
+                .setRequired(true)),
+        async execute(interaction) {
+            const randomNumber = Math.floor(Math.random() * 10) + 1;
+            let attempts = 3;
 
-     let attempts = 3;
-
-     while (attempts > 0) {
-            const filter = (m) => {
-                if (m.author.id !== interaction.user.id) return false;
-                const int = parseInt(m.content);
-                return !isNaN(int) && int >= 1 && int <= 10;
-            };
-            const message = await interaction.reply("Guess a number between 1 and 10");
-            
-            const guess = await interaction.options.getString({
-                filter,
-                max: 1,
-                time: 10000,
-                errors: ['time']
-
-            });
-            if (guess.size === 0)
-                 await interaction.editReply("You didn't reply in time!");
-                break;
-            }
-            const userChoice = parseInt(guess.first().content);
-            if (userChoice === randomNumber) {
-                await interaction.editReply("You got it right!");
-            } else {
-                await interaction.editReply(`You got it wrong! The number was ${randomNumber}`);
-                attempts--;
-                if (attempts > 0) {
-                    await interaction.editReply(`You have ${attempts} attempts left!`);
+            await interaction.reply('Guess a number between 1 and 10!');
+          
+            while (attempts > 0) {
+                const userNumber = interaction.options.getString('number');
+                const Number = parseInt(userNumber);
+                if (Number === randomNumber) {
+                    await interaction.reply('You guessed the number!' + randomNumber  + 'was the number');
+                    break;
                 } else {
-                    await interaction.editReply("You have no attempts left!");
+                    attempts--;
+                    await interaction.reply(`That was wrong! You have ${attempts} attempts left.`);
                 }
             }
-        }
-    }
-
+        },
+};
+            
+              
